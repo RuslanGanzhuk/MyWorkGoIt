c3e91fecb2c606ec379dcc942e3e597513059762
RuslanGanzhuk <hanzhukruslan@gmail.com>
2016-05-20 23:56:07 +0300

    Add files via upload

---
+++ FirstHomeWork.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Родион on 20.05.2016.
+ */
+public class FirstHomeWork {
+    public static void main(String[] arg) {
+        System.out.println("\"Hi, I'm your first Java program! Stay tuned!\"");
+    }
+}
